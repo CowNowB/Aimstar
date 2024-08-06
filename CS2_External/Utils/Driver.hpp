59f4e32d8224c886509551f94d25ad3da22ba217
CowNowB <1945994694@qq.com>
2024-08-06 16:50:51 +0800

    Add files via upload

---
+++ CS2_External/Utils/Driver.hpp
@@ -0,0 +1,136 @@
+#pragma once
+#include <cstdint>
+#include <cstdio>
+#include <libloaderapi.h>
+#include <string>
+#include <vadefs.h>
+constexpr ULONG ioctl_call_driver = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x775, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+
+class _driver
+{
+private:
+	HANDLE _driver_handle;
+	int _processid;
+	ULONG64 _clientaddress;
+	ULONG64 _engineaddress;
+
+
+	//request codes
+#define DRIVER_READVM				0x80000001
+#define CLIENT_BASE					0x80000002
+#define WRITE						0x80000003
+#define ENGINE_BASE					0x80000004
+#define INPUT_BASE					0x80000005
+
+	struct _requests
+	{
+		//rw
+		uint32_t    src_pid;
+		uint64_t    src_addr;
+		uint64_t    dst_addr;
+		size_t        size;
+
+		//function requests
+		int request_key;
+
+		ULONG64 client_base;
+		ULONG64 engine_base;
+	};
+
+	auto readvm(uint32_t src_pid, uint64_t src_addr, uint64_t dst_addr, size_t size) -> void
+	{
+		if (src_pid == 0 || src_addr == 0) return;
+
+		_requests out = { src_pid, src_addr, dst_addr, size, DRIVER_READVM };
+		DeviceIoControl(_driver_handle, ioctl_call_driver, &out, sizeof(out), nullptr, 0, nullptr, nullptr);
+	}
+	auto writevm(uint32_t src_pid, uint64_t src_addr, uint64_t dst_addr, size_t size) -> void
+	{
+		if (src_pid == 0 || dst_addr == 0) return;
+
+		_requests out = { src_pid, src_addr, dst_addr, size, WRITE };
+		DeviceIoControl(_driver_handle, ioctl_call_driver, &out, sizeof(out), nullptr, 0, nullptr, nullptr);
+	}
+public:
+	auto initdriver(int processid) -> void
+	{
+		_driver_handle = CreateFileA("\\\\.\\AimStarDriver", GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr); //get a handle to our driver
+		if (_driver_handle != INVALID_HANDLE_VALUE)
+		{
+			_processid = processid;
+		}
+	}
+
+
+
+	void readsize(const uintptr_t address, const void* buffer, const size_t size)
+	{
+		readvm(_processid, address, uintptr_t(buffer), size);
+	}
+
+
+	std::string read_str(std::uintptr_t dst)
+	{
+		char buf[256];
+		readsize(dst, &buf, sizeof(buf));
+		return buf;
+	}
+
+	template <typename T>
+	T readv(uintptr_t src, size_t size = sizeof(T))
+	{
+		T buffer;
+		readvm(_processid, src, (uintptr_t)&buffer, size);
+		return buffer;
+	}
+
+	template <typename T>
+	T readteste(uintptr_t src, size_t size = sizeof(T))
+	{
+		T buffer;
+		readvm(_processid, src, (uintptr_t)&buffer, size);
+		return buffer;
+	}
+
+	template <typename T>
+	void write(const uintptr_t address, const T& buffer)
+	{
+		writevm(_processid, address, (uintptr_t)&buffer, sizeof(T));
+	}
+	template <typename T>
+	void write(const uintptr_t address, const T& buffer, int Size)
+	{
+		writevm(_processid, address, (uintptr_t)&buffer, Size);
+	}
+	auto client_address() -> ULONG64
+	{
+		_requests out = { 0 };
+		out.request_key = CLIENT_BASE;
+		out.src_pid = _processid;
+		DeviceIoControl(_driver_handle, ioctl_call_driver, &out, sizeof(out), &out, sizeof(out), nullptr, nullptr);
+		_clientaddress = out.client_base;
+		return out.client_base;
+	}
+
+	auto engine_address() -> ULONG64
+	{
+		_requests out = { 0 };
+		out.request_key = ENGINE_BASE;
+		out.src_pid = _processid;
+		DeviceIoControl(_driver_handle, ioctl_call_driver, &out, sizeof(out), &out, sizeof(out), nullptr, nullptr);
+		_engineaddress = out.client_base;
+		return out.client_base;
+	}
+
+	auto input_address() -> ULONG64
+	{
+		_requests out = { 0 };
+		out.request_key = INPUT_BASE;
+		out.src_pid = _processid;
+		DeviceIoControl(_driver_handle, ioctl_call_driver, &out, sizeof(out), &out, sizeof(out), nullptr, nullptr);
+		_engineaddress = out.client_base;
+		return out.client_base;
+	}
+};
+
+inline _driver driver;
---
+++ CS2_External/Utils/Initial/Init.h
@@ -0,0 +1,112 @@
+﻿#pragma once
+#include <fstream>
+#include <tchar.h>
+#include <shellapi.h>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+#include "system.h"
+
+namespace Init
+{
+    using namespace std;
+
+	class Verify
+	{
+	public:
+		// Check if the Windows version is higher than 7
+		static bool CheckWindowVersion() {
+            OSVERSIONINFOEX osvi;
+            ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+            osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+            osvi.dwMajorVersion = 6;
+            osvi.dwMinorVersion = 1;
+
+            ULONGLONG conditionMask = VerSetConditionMask(0, VER_MAJORVERSION, VER_GREATER_EQUAL);
+            conditionMask = VerSetConditionMask(conditionMask, VER_MINORVERSION, VER_GREATER_EQUAL);
+
+            if (VerifyVersionInfo(&osvi, VER_MAJORVERSION | VER_MINORVERSION, conditionMask))
+            {
+                return true;
+            }
+            return false;
+		}
+
+        static bool isVerified(std::string fileName)
+        {
+            ifstream infile(fileName);
+            if (infile.good())
+                return true;
+            else
+                return false;
+        }
+
+        //otp code verify by @_ukia_
+        static void CodeGenerate(string& time, string& code) {
+            auto now = chrono::system_clock::now();
+            auto now_utc = chrono::system_clock::to_time_t(now);
+            struct tm tm_utc;
+            gmtime_s(&tm_utc, &now_utc);
+            int year = tm_utc.tm_year + 1900;
+            int month = tm_utc.tm_mon + 1;
+            int day = tm_utc.tm_mday;
+            int hour = tm_utc.tm_hour;
+            int minute = tm_utc.tm_min;
+            int sum = year + month + day + hour + minute;
+            int otp = sum ^ 3351 % 10000;
+            time = to_string(year) + "-" + to_string(month) + "-" + to_string(day) + "-" + to_string(hour) + "-" + to_string(minute);
+            code = to_string(otp);
+        }
+	};
+
+    class Client
+    {
+    public:
+        // Get the maximum framerate with vsync
+        static int getMaxFrameRate() {
+            HDC hdc = GetDC(NULL);
+            int rate = GetDeviceCaps(hdc, VREFRESH);
+            ReleaseDC(NULL, hdc);
+            return rate;
+        }
+
+        // Get the HWID
+        static std::string GenerateHWID() {
+            std::string macAddress = GetMACAddress();
+            std::string serialNum = GetHardDiskSerial();
+
+            return macAddress + serialNum;
+        }
+
+        // Check if the game window is activated
+        static bool isGameWindowActive() {
+            HWND hwnd_cs2 = FindWindowA(NULL, "Counter-Strike 2");
+            HWND hwnd_perfectworld = FindWindowA(NULL, "\u53cd\u6050\u7cbe\u82f1\uff1a\u5168\u7403\u653b\u52bf");
+
+            if (hwnd_cs2 != NULL || hwnd_perfectworld != NULL) {
+                HWND foreground_window = GetForegroundWindow();
+                if (foreground_window == hwnd_cs2 || foreground_window == hwnd_perfectworld) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void SleepGameTick(int TickCount)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(15 * TickCount));
+        }
+
+        static void Exit()
+        {
+            exit(0);
+        }
+
+        static void QuitGame()
+        {
+            ShellExecuteA(NULL, "open", "cmd.exe", "/C taskkill /F /IM cs2.exe", NULL, SW_HIDE);
+        }
+    };
+
+}
+
