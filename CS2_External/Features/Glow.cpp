03e321022650f8b1ebf415c846865a4e0fd7399d
CowNowB <1945994694@qq.com>
2024-08-13 17:51:27 +0800

    ChromaGlow

--- CS2_External/Features/Glow.cpp
+++ CS2_External/Features/Glow.cpp
@@ -1,11 +1,12 @@
 #include "Glow.h"
 #include <thread>
+#define PI 3.141592653
 
 void Glow::Run(const CEntity& Entity)
 {
 	int on = 1;
 	int off = 0;
-	DWORD color = MiscCFG::GlowColor;
+	DWORD color = Glow::rainbow();
 	if (!MiscCFG::EnemySensor)
 	{
 		ProcessMgr.WriteMemory(Entity.Pawn.Address + Offset::Entity.GlowFunction, off);
@@ -15,4 +16,15 @@ void Glow::Run(const CEntity& Entity)
 		ProcessMgr.WriteMemory(Entity.Pawn.Address + Offset::Entity.GlowColorOverride, color);
 	}
 	return;
+}
+
+ImColor Glow::rainbow() {
+	static float time = 0.0f;
+	time += 0.001f;
+
+	float r = (std::sin(time * 2.0f * PI) + 1.0f) * 0.5f;
+	float g = (std::sin(time * 2.0f * PI + 2.0f * PI / 3.0f) + 1.0f) * 0.5f;
+	float b = (std::sin(time * 2.0f * PI + 4.0f * PI / 3.0f) + 1.0f) * 0.5f;
+
+	return ImColor(r, g, b, 200.f);
 }
